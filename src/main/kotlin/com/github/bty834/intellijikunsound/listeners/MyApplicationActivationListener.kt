cc3cb38460e06a3b3a98056066368613c7c2d9d6
GitHub Action <action@github.com>
2024-06-13 15:58:27 +0000

    Template cleanup

--- src/main/kotlin/com/github/bty834/intellijikunsound/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bty834/intellijikunsound/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bty834.intellijikunsound.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/bty834/intellijikunsound/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/bty834/intellijikunsound/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.bty834.intellijikunsound.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.bty834.intellijikunsound.MyBundle
+import com.github.bty834.intellijikunsound.services.MyProjectService
 import javax.swing.JButton
 
 
